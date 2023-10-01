4d24ac115ee31ce18897ca22dc14940d4d845931
Paulo Rocha <pauloyr@outlook.com>
2023-10-01 12:53:56 -0300

    add login page

---
+++ page/login/index.tsx
@@ -0,0 +1,12 @@
+import { NextComponentType } from "next";
+
+const LoginPage: NextComponentType = () => {
+    return (
+        <>
+            <h1>Login</h1>
+        </>
+    )
+}
+
+
+export default LoginPage
