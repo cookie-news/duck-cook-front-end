c884db05e876645bd2b31649099f16fac4c366aa
LuidyC3C <luidy.moura525@gmail.com>
2023-10-05 18:11:06 -0300

    Test Auth login v3

--- src/controllers/login/controller.tsx
+++ src/controllers/login/controller.tsx
@@ -1,11 +1,13 @@
 export const callLoginAuth = async (loginInfo:any) => {
     try {
+        console.log(loginInfo);
 		const res = await fetch(`https://duck-cook-auth-ms.onrender.com/v1/auth/login`, {
             method: 'POST',
             headers: {
                 'Content-type': 'application/json'
             },
-            body: JSON.stringify(loginInfo)
+            body: JSON.stringify(loginInfo),
+            mode: 'same-origin'
         });
 		const data = await res.json();
 		console.log(data);
