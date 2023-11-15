00c650d1bd53d689a88f39d2485583dd9b6f80b0
Davi Castro <davifgeo@gmail.com>
2023-11-15 15:42:52 -0300

    Visualização do usuário

--- src/app/user/view/page.tsx
+++ src/app/user/view/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { NextPage } from "next";
-import { useState } from "react";
+import { useState, useContext } from "react";
 
 //Material UI
 import Carousel from 'react-material-ui-carousel'
@@ -14,27 +14,19 @@ import ErrorMessages from "@utils/ErrorMessages";
 
 //Types
 import RecipeType from "@/types/RecipeType";
+import { User } from "@root/src/data/user.service";
+
+//Data services
+import { AuthContext } from "@context/AuthContext";
 
 const ViewRecipePage: NextPage = () => {
-    const [user, setUser] = useState({
-        id: 'test',
-        fullName: 'Luidy Moura',
-        userName: 'luidymg',
-        email: 'luidy.mourawm@gmail.com',
-        avatar: 'https://imgs.search.brave.com/VtUkZ_UfRNwFfcSEtdUTd-7tIBHE7FiIJdKsX0Vjirg/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/ZnJlZS1wc2QvM2Qt/aWxsdXN0cmF0aW9u/LWh1bWFuLWF2YXRh/ci1wcm9maWxlXzIz/LTIxNTA2NzExNDIu/anBnP3NpemU9NjI2/JmV4dD1qcGc'
-    });
+
+    //User
+    const { userData } = useContext(AuthContext);
 
     const [fiveRecentsUserRecipes, setFiveRecetsUserRecipes] = useState([]);
 
-    const [userRecipeLiked, setUserRecipeLiked] = useState([{
-        title: 'Receita 1',
-        createdDate: '07/11/2023 02:25',
-        images: [{
-            src: 'https://imgs.search.brave.com/09u0egwEI5oo55tfE80XFTtFQlEX7GhbFl18TXkyGfo/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jbGFz/c2ljLmV4YW1lLmNv/bS93cC1jb250ZW50/L3VwbG9hZHMvMjAy/Mi8wNS9SZW1hbnNv/LWRvLVBlaXhlLmpw/Zz9xdWFsaXR5PTcw/JnN0cmlwPWluZm8m/dz0xMDI0'
-        }],
-        commentsNumber: 10,
-        likeNumber: 5
-    }]);
+    const [userRecipeLiked, setUserRecipeLiked] = useState<RecipeType[]>([]);
 
     // react hook form
     const { register, handleSubmit, formState: { errors }, reset } = useForm();
@@ -44,11 +36,11 @@ const ViewRecipePage: NextPage = () => {
             <div className="flex pb-4 flex-col md:flex-row items-center	md:items-center" style={{ borderBottom: '1px solid #494949' }}>
                 <Avatar
                     sx={{ width: 100, height: 100 }}
-                    src={user.avatar}
+                    src={userData.image}
                     className="mr-0 md:mr-8"
                 />
                 <Typography color="text.primary" variant="h3">
-                    <b>{user.fullName}</b>
+                    <b>{userData.name}</b>
                 </Typography>
             </div>
             <div className="p-0 pt-4 md:p-4">
@@ -61,19 +53,19 @@ const ViewRecipePage: NextPage = () => {
                             <b>Nome de Completo:</b>
                         </Typography>
                         <Typography color="text.primary" variant="body2">
-                            {user.fullName}
+                            {userData.name}
                         </Typography>
                         <Typography color="text.primary" variant="body1" className="mt-2">
                             <b>Nome de Usuário:</b>
                         </Typography>
                         <Typography color="text.primary" variant="body2">
-                            {user.userName}
+                            {userData.user}
                         </Typography>
                         <Typography color="text.primary" variant="body1" className="mt-2">
                             <b>Email:</b>
                         </Typography>
                         <Typography color="text.primary" variant="body2">
-                            {user.email}
+                            {userData.email}
                         </Typography>
                     </div>
                     <div className="flex justify-center md:justify-end flex-col md:flex-row h-9 gap-2 mt-4 mb-4 md:mt-0 md:mb-0">
@@ -129,7 +121,7 @@ const ViewRecipePage: NextPage = () => {
                                                         <CardMedia
                                                             component="img"
                                                             sx={{ width: 80 }}
-                                                            image={recipe.images[0]}
+                                                            image={recipe.images[index]}
                                                             style={{ borderRadius: '0px 1px 1px 0px' }}
                                                         />
                                                         :
@@ -167,7 +159,7 @@ const ViewRecipePage: NextPage = () => {
                                                         <CardMedia
                                                             component="img"
                                                             sx={{ width: 80 }}
-                                                            image={recipe.images[0].src}
+                                                            image={recipe.images[0]}
                                                             style={{ borderRadius: '0px 1px 1px 0px' }}
                                                         />
                                                         :
