9fdbb87367518dfbbe262441e852338b4dd2a34d
Lucas <lucasjoao85@gmail.com>
2023-11-21 23:10:00 -0300

    remove more likeds

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,14 +13,6 @@ function PageRoot() {
     <PageWrapper isProtected={false} hasMenu>
       <div className="flex flex-col gap-8 mt-16 px-4 md:px-0">
         <p className="bg-green-800 p-3 text-white uppercase w-fit font-bold">
-          RECEITAS MAIS CURTIDAS
-        </p>
-        <section className="flex gap-[0.4rem] flex-wrap">
-          <React.Suspense fallback={<SkeletonFallbackMoreLikeds />}>
-            <MoreLikedsList />
-          </React.Suspense>
-        </section>
-        <p className="bg-green-800 p-3 text-white uppercase w-fit font-bold">
           Feed de receitas
         </p>
         <section className="flex flex-col items-end gap-2">
