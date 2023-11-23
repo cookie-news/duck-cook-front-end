2b4590e1d730fa956e08c25812db71cd48f3c343
Paulo <pauloyr@outlook.com>
2023-11-23 02:05:30 -0300

    add border in text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,13 +11,13 @@ function PageRoot() {
     return (
         <PageWrapper isProtected={false} hasMenu>
             <div className="flex flex-col gap-8 mt-16 px-4 md:px-0">
-                <p className="bg-green-800 p-3 text-white uppercase w-fit font-bold">
+                <p className="bg-green-800 p-3 text-white uppercase w-fit font-bold rounded-md">
                     Receitas mais curtidas
                 </p>
                 <section>
                     <MoreLikedsList />
                 </section>
-                <p className="bg-green-800 p-3 text-white uppercase w-fit font-bold">
+                <p className="bg-green-800 p-3 text-white uppercase w-fit font-bold rounded-md">
                     Feed de receitas
                 </p>
                 <section className="flex flex-col items-end gap-2">
